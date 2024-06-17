1bf52c35a4cd1fbe4506ed85d3aea499880e3507
nathanieladiah <nathanieladiah@gmail.com>
2024-06-17 12:02:45 -0400

    Add logout on dropdown

--- src/components/header/AppHeaderDropdown.js
+++ src/components/header/AppHeaderDropdown.js
@@ -1,4 +1,16 @@
-import React from 'react'
+import {
+  cilAccountLogout,
+  cilBell,
+  cilCommentSquare,
+  cilCreditCard,
+  cilEnvelopeOpen,
+  cilFile,
+  cilLockLocked,
+  cilSettings,
+  cilTask,
+  cilUser,
+} from '@coreui/icons'
+import CIcon from '@coreui/icons-react'
 import {
   CAvatar,
   CBadge,
@@ -9,22 +21,19 @@ import {
   CDropdownMenu,
   CDropdownToggle,
 } from '@coreui/react'
-import {
-  cilBell,
-  cilCreditCard,
-  cilCommentSquare,
-  cilEnvelopeOpen,
-  cilFile,
-  cilLockLocked,
-  cilSettings,
-  cilTask,
-  cilUser,
-} from '@coreui/icons'
-import CIcon from '@coreui/icons-react'
+import React from 'react'
 
+import { useNavigate } from 'react-router-dom'
+import { auth } from '../../firebase.config'
 import avatar8 from './../../assets/images/avatars/8.jpg'
 
 const AppHeaderDropdown = () => {
+  const navigate = useNavigate()
+
+  const onLogout = () => {
+    auth.signOut()
+    navigate('/login')
+  }
   return (
     <CDropdown variant="nav-item">
       <CDropdownToggle placement="bottom-end" className="py-0 pe-0" caret={false}>
@@ -87,6 +96,10 @@ const AppHeaderDropdown = () => {
         <CDropdownItem href="#">
           <CIcon icon={cilLockLocked} className="me-2" />
           Lock Account
+        </CDropdownItem>
+        <CDropdownItem onClick={onLogout}>
+          <CIcon icon={cilAccountLogout} className="me-2" />
+          Log Out
         </CDropdownItem>
       </CDropdownMenu>
     </CDropdown>
